30591969aa12abe6c9d5be2c3db12f302d94810d
Yeison <187584546+SYeison@users.noreply.github.com>
2024-11-10 16:06:58 -0500

    pasamos la animacionde splash a login

---
+++ webGimnasio/webGimnasio/Paginas/Scripts/frmSplash.js
@@ -0,0 +1,5 @@
+﻿// Esperamos 2.5 segundos para hacer la redirección
+setTimeout(function () {
+    // Cambiar a la nueva página después de la animación
+    window.location.href = "frmlogin.html";  // Reemplaza con la URL deseada
+}, 2500); // Redirige después de 2.5 segundos
