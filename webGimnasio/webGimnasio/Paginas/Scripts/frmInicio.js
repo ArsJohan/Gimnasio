49a3291385dc0d4f244514f1a684a526f17fcb0c
ArsJohan <133719384+ArsJohan@users.noreply.github.com>
2024-11-11 18:08:20 -0500

    Se agregaron los script del frm login e inicio, para la comunicacíón con la Api

--- webGimnasio/webGimnasio/Paginas/Scripts/frmInicio.js
+++ webGimnasio/webGimnasio/Paginas/Scripts/frmInicio.js
@@ -1,7 +1,32 @@
-﻿
+﻿let dir = "";
 jQuery(function () {
     //Carga el menú
     $("#dvMenu").load("../Paginas/Menu.html");
-
+    let codigo = window.location.search.substr(1);
+    buscarNombre(codigo,"#col-2");
+    
 });
 
+async function buscarNombre(codigo,divnombre) {
+
+   try {
+        const Respuesta = await fetch(dir+"Empleado?codigo="+codigo,
+            {
+                method: "GET",
+                mode: "cors",
+                headers: { "content-type": "application/json", }
+            }
+        );
+        const Rpta = await Respuesta.json();
+       
+        $(divNombre).empty();
+      
+        $(divnombre).add('<h4>' + Rpta.Nombre+'</h4>');
+        
+        return "Termino";
+    }
+    catch (error) {
+        return error;
+    }
+
+}
--- webGimnasio/webGimnasio/Paginas/Scripts/frmLogin.js
+++ webGimnasio/webGimnasio/Paginas/Scripts/frmLogin.js
@@ -1,10 +1,62 @@
-﻿jQuery(function () {
+﻿let dir = "";
+jQuery(function () {
 
-    $("#btnIngresar").on("click", function () {
-        //Simple
-        window.location.href = "frmInicio.html";
+    
+    function mensajeError(texto) {
+        $("#dvMensaje").removeClass("alert alert-success");
+        $("#dvMensaje").addClass("alert alert-danger");
+        $("#dvMensaje").html(texto);
+    }
+    function mensajeInfo(texto) {
+        $("#dvMensaje").removeClass("alert alert-success");
+        $("#dvMensaje").addClass("alert alert-info");
+        $("#dvMensaje").html(texto);
+    }
+    function mensajeOk(texto) {
+        $("#dvMensaje").removeClass("alert alert-success");
+        $("#dvMensaje").addClass("alert alert-success");
+        $("#dvMensaje").html(texto);
+    }
+    $("#btnIngresar").on("click", async function () {
 
+
+
+        try {
+            let clave = $("#user_clave").val();
+            let pass = $("#user_password").val();
+
+            const datosOut = await fetch(dir + "login?clave=" + clave,
+                {
+                    method: "GET",
+                    mode: "cors",
+                    headers: {
+                        "content-type": "application/json",
+
+                    }
+
+                }
+            );
+            const datosIn = await datosOut.json();
+            if (datosIn == "") {
+                mensajeError("Clave o Contraseña erroneos");
+                return;
+            }
+
+            let api_pass = datosIn.Contrasena;
+            if (pass == api_pass) {
+                window.location.href = "frmInicio.html?" + datosIn.Codigo;
+                return;
+
+            }
+            else {
+                mensajeError("Clave o Contraseña erroneos");
+                return;
+            }
+
+        } catch (error) {
+            mensajeError("Error: " + error);
+        }
     });
-
+       
 
 });
